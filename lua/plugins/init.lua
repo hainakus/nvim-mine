432265303e5ec2e9e1de8c93090763e020e81ec7
Tiago Mendes <Tiago.Mendes_ext@a-to-be.com>
2025-03-02 00:20:21 +0000

    all

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,22 +22,80 @@ return {
   event = 'VeryLazy',
   opts = true,
 },
-{
-  "stsewd/spotify.nvim",
-  build = ":UpdateRemotePlugins",
-  config = function()
-    require("spotify").setup()
-  end,
-  init = function()
-    -- Optional mappings.
-    vim.keymap.set("n", "<leader>ss", ":Spotify play/pause<CR>", { silent = true })
-    vim.keymap.set("n", "<leader>sj", ":Spotify next<CR>", { silent = true })
-    vim.keymap.set("n", "<leader>sk", ":Spotify prev<CR>", { silent = true })
-    vim.keymap.set("n", "<leader>so", ":Spotify show<CR>", { silent = true })
-    vim.keymap.set("n", "<leader>sc", ":Spotify status<CR>", { silent = true })
-  end,
-}
+ {
+  "gerazov/ollama-chat.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "stevearc/dressing.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  -- lazy load on command
+  cmd = {
+    "OllamaQuickChat",
+    "OllamaCreateNewChat",
+    "OllamaContinueChat",
+    "OllamaChat",
+    "OllamaChatCode",
+    "OllamaModel",
+    "OllamaServe",
+    "OllamaServeStop"
+  },
 
+  keys = {
+    {
+      "<leader>ocq",
+      "<cmd>OllamaQuickChat<cr>",
+      desc = "Ollama Quick Chat",
+      mode = { "n", "x" },
+      silent = true,
+    },
+    {
+      "<leader>ocn",
+      "<cmd>OllamaCreateNewChat<cr>",
+      desc = "Create Ollama Chat",
+      mode = { "n", "x" },
+      silent = true,
+    },
+    {
+      "<leader>occ",
+      "<cmd>OllamaContinueChat<cr>",
+      desc = "Continue Ollama Chat",
+      mode = { "n", "x" },
+      silent = true,
+    },
+    {
+      "<leader>och",
+      "<cmd>OllamaChat<cr>",
+      desc = "Chat",
+      mode = { "n" },
+      silent = true,
+    },
+    {
+      "<leader>ocd",
+      "<cmd>OllamaChatCode<cr>",
+      desc = "Chat Code",
+      mode = { "n" },
+      silent = true,
+    },
+  },
+
+  opts = {
+    chats_folder = vim.fn.stdpath("data"), -- data folder is ~/.local/share/nvim
+    -- you can also choose "current" and "tmp"
+    quick_chat_file = "ollama-chat.md",
+    animate_spinner = true,  -- set this to false to disable spinner animation
+    model = "openhermes2-mistral",
+    model_code = "codellama",
+    url = "http://127.0.0.1:11434",
+    serve = {
+      on_start = false,
+      command = "ollama",
+      args = { "serve" },
+      stop_command = "pkill",
+      stop_args = { "-SIGTERM", "ollama" },
+    },
+  }
+},
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
